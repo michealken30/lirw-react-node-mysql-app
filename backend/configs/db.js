cd3177464fb7d143db290ca26f79f5be52327ff3
michealken30 <kenneth.oshogwe@gmail.com>
2024-10-24 18:26:17 +0100

    Update db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
    host: 'localhost',
    port: '3306',
-   user: 'root',
-   password: '12345678',
+   user: 'appuser',
+   password: 'learnIT02#',
    database: 'react_node_app'
 });
 
